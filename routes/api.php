3a56ea3aee01347d0b41660d2af5393e61488e4f
laupwing <laupwing@gmail.com>
2025-10-02 15:17:32 +0200

    Add workout logging, history, and summary routes to API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\WorkoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('/workouts/log', [WorkoutController::class, 'log']);
+Route::get('/workouts/history', [WorkoutController::class, 'history']);
+Route::get('/workouts/summary', [WorkoutController::class, 'summary']);
+// Route::get('/exercises/search', [ExerciseController::class, 'search']);
