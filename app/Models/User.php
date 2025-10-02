a8e93419efbe76137b98e814b29c0a448a68bf29
laupwing <laupwing@gmail.com>
2025-10-02 11:39:08 +0200

    Refactor User model to update fillable attributes and remove commented code

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,22 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        // 'email',
+        // 'password',
+        'whatsapp_number',
         'name',
-        'email',
-        'password',
+        'gender',
+        'age',
+        'height_cm',
+        'current_weight_kg',
+        'target_weight_kg',
+        'fitness_goal',
+        'experience_level',
+        'preferences',
+        'streak_days',
+        'last_workout_date',
+        'is_active',
+        'onboarded_at',
     ];
 
     /**
@@ -29,10 +42,10 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    // protected $hidden = [
+    //     'password',
+    //     'remember_token',
+    // ];
 
     /**
      * Get the attributes that should be cast.
@@ -42,8 +55,26 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            // 'email_verified_at' => 'datetime',
+            // 'password' => 'hashed',
+            'preferences' => 'array',
+            'last_workout_date' => 'date',
+            'onboarded_at' => 'datetime',
         ];
+    }
+
+    public function workouts()
+    {
+        return $this->hasMany(Workout::class);
+    }
+
+    public function conversations()
+    {
+        return $this->hasMany(Conversation::class);
+    }
+
+    public function progressMetrics()
+    {
+        return $this->hasMany(ProgressMetric::class);
     }
 }
